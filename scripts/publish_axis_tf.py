cda5303e19a647e1a284ef1d7ed1b78bd763c94c
scarlettfres <scarlett_fres@hotmail.com>
2015-07-08 15:33:29 +0200

    launch cam in an given position

--- scripts/publish_axis_tf.py
+++ scripts/publish_axis_tf.py
@@ -4,36 +4,49 @@ import rospy
 import math
 from axis_camera.msg import Axis
 import tf
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
-base_name = "/axis_camera_fin"
+base_name = "/axis_camera"
 base_frame = "/axis_camera_init"
 broadcaster = tf.TransformBroadcaster()
+PAN = -29.9032001495
+TILT = -69.8181991577
+
 
 def axis_cb(data):
 
-     
-    global broadcaster, base_frame, base_name
-    # il y avait un plus pi ici 
-    pan =  data.pan * math.pi / 180.
-    tilt = -data.tilt * math.pi / 180.
-    q = quaternion_from_euler(0,0,pan)
+    listener = tf.TransformListener()  # ????
+
+    print '111111111'
+    # (trans, rot) = listener.lookupTransform(
+    #     "/axis_camera_init", '/map', rospy.Time(0))
+
+    rot = (0.6287990110164235, 0.7480819940486101, -0.1563927862398917,
+           -0.14327047964303885)
+
+    print "22222"
+    # il y avait un plus pi ici
+
+    pan = (data.pan - PAN) * math.pi / 180.
+    tilt = -(data.tilt - TILT) * math.pi / 180.
+    print "pan = ", pan
+    print "tilt", tilt
+    euler = euler_from_quaternion(rot)
+    euler_modif = (euler[0] - tilt, euler[1], euler[2] - pan)
+    q = quaternion_from_euler(euler_modif[0], euler_modif[1], euler_modif[2])
     now = rospy.Time.now()
-    broadcaster.sendTransform((0,0,0),
-            q,now,base_name+"/pan",base_frame)
-    q = quaternion_from_euler(0,tilt,0)
-    broadcaster.sendTransform((0,0,0),
-            q,now,base_name,base_name+"/pan")
+    broadcaster.sendTransform((0.6251185950728501, -0.9568288250572693, 2.5808307217613855),
+                              q, now, base_name, "/map")
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node("axis_tf_broadcaster")
-        base_frame = rospy.get_param("~base_frame",base_frame)
-        base_name = rospy.get_param("~base_name",base_name)
-        #print base_name, base_frame
-        listener = tf.TransformListener()
-        axis_sub = rospy.Subscriber("/state",Axis,axis_cb)
+        # base_frame = rospy.get_param("~base_frame", base_frame)
+        # base_name = rospy.get_param("~base_name", base_name)
+        # print base_name, base_frame
+
+        axis_sub = rospy.Subscriber("/state", Axis, axis_cb)
         rospy.loginfo("Started Axis TF broadcaster")
         rospy.spin()
     except rospy.ROSInterruptException:
--- scripts/teleop_axis.py
+++ scripts/teleop_axis.py
@@ -3,10 +3,14 @@
 import rospy
 from geometry_msgs.msg import Twist
 from axis_camera.msg import Axis
-import sys, select, termios, tty
+import sys
+import select
+import termios
+import tty
 import time
 print " i^, j<- ,k->, l!, Q to end "
 time.sleep(2)
+
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -20,56 +24,55 @@ def getKey():
     return key
 
 
-def vels(speed,turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed, turn)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
-    
+
     rospy.init_node('_teleop')
     pub = rospy.Publisher('cmd', Axis, queue_size=5)
     state = Axis()
-    state.pan=0
-    state.tilt=-45
-
+    state.pan = 0
+    state.tilt = -45
 
     pub.publish(state)
     try:
         while(1):
-			key = getKey()
-			print key
-			if key == "i":
-				state.tilt+=10
-				if state.tilt>-5: 
-					state.tilt=-5
-				print state
-				pub.publish(state)
-			elif key == "k":
-				state.tilt+=-10
-				if state.tilt<-85: 
-					state.tilt=-85
-				print state
-				pub.publish(state)
-			elif key == "j":
-				state.pan+=-10
-				if state.pan<-175: 
-					state.tilt=-175
-				print state
-				pub.publish(state)
-			elif key == "l":
-				state.pan+=10
-				if state.pan>175: 
-					state.tilt=175
-				print state
-				pub.publish(state)
-			elif key == "q":
-				break
-         	
+            key = getKey()
+            print key
+            if key == "i":
+                state.tilt += 10
+                if state.tilt > -5:
+                    state.tilt = -5
+                print state
+                pub.publish(state)
+            elif key == "k":
+                state.tilt += -10
+                if state.tilt < -85:
+                    state.tilt = -85
+                print state
+                pub.publish(state)
+            elif key == "j":
+                state.pan += -10
+                if state.pan < -175:
+                    state.tilt = -175
+                print state
+                pub.publish(state)
+            elif key == "l":
+                state.pan += 10
+                if state.pan > 175:
+                    state.tilt = 175
+                print state
+                pub.publish(state)
+            elif key == "q":
+                break
+
     except:
         print e
 
     finally:
-    	print state
+        print state
         pub.publish(state)
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
